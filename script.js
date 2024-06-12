e645c414056d37eb2dd5cc9f30eb3c52247cca1b
Jhay-Great <alemonai.jenef@gmail.com>
2024-06-12 16:07:15 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+'use script'
+const addTodoBtn = document.querySelector('.create_activity');
+const todoForm = document.querySelector('.todo_activity_form');
+
+/**create todo functionality
+ * when create button is click
+ * add todo activity in todo container
+ * display a custom alert popup as 'activity created' if successful and 'activity not created' if failed
+ */
+const todoActivitiesArray = [];
+
+// setting a min date
+/**setting min date
+ * select the date input
+ * get the current day's date
+ * set a min attribute to the date input to the current date
+ */
+
+const today = new Date();
+console.log(today);
+const dateInput = document.querySelector('#todo_activity_time-elapsed')
+// dateInput.setAttribute('min', today);
+dateInput.min = today;
+
+
+todoForm.addEventListener('submit', function(e) {
+    e.preventDefault(e);
+    
+    // selecting all values from input fields
+    const inputFields = document.querySelectorAll('input');
+    // looping through the input fields
+    const currentTodoActivity = {};
+    inputFields.forEach(field => {
+        currentTodoActivity[field.name] = field.value;
+        
+    })
+    todoActivitiesArray.push(currentTodoActivity);
+    console.log(todoActivitiesArray);
+})
